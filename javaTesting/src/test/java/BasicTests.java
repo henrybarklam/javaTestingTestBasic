ee55e34b1146a54299c7bcc8c221feb550c164bf
Henry Opto <henryopto@Henrys-MacBook-Pro.local>
2023-07-10 20:42:04 +0100

    First test written

---
+++ javaTesting/src/test/java/BasicTests.java
@@ -0,0 +1,13 @@
+import org.example.Basics;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BasicTests {
+    //Tests going to go in here
+    @Test
+    public void compare(){
+        Basics basicTests = new Basics();
+        int value = basicTests.compare(2,1);
+        Assertions.assertEquals(1,value);
+    }
+}
